30770d3d4c5d7b7f383b1a752a550ae47d647b07
Phantom <abracadabra1253@gmail.com>
2023-11-17 00:33:33 +0400

    .......

---
+++ components/modules/hehe.js
@@ -0,0 +1,14 @@
+function hehe() {
+
+	console.log(`hehe`)
+	
+}
+
+export function hehe2() {
+
+	console.log(`hehe2`)
+	
+}
+
+
+export { hehe } 
---
+++ components/modules/main.js
@@ -0,0 +1,42 @@
+//import  { hehe, hehe2 }   from "./hehe.js";
+
+//console.log(`test`)
+
+
+//hehe()
+//hehe2()
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", ()=> {
+	
+var qwlq11more = document.querySelector("#more_zzq")	
+	
+var qwqe = document.querySelectorAll(".kkkql123_fmore2")[0]
+
+qwlq11more.onclick = () => {
+	
+qwqe.style.display = 'block';
+document.body.style.overflow = 'hidden';
+	
+};
+
+
+
+var qwlq11morclqlqse = document.querySelector("#kclql_close")	
+
+
+
+qwlq11morclqlqse.onclick = () => {
+	
+	qwqe.style.display = 'none';
+	document.body.style.overflow = '';
+	
+};
+
+
+
+
+})
